5e8ba13ae42f8c19d58dd087ec2feda3fdc97af1
abha_genecorp <abha.c06@gmail.com>
2017-09-11 14:01:47 +0530

    Displaying selected image position on click of next button

--- app/src/main/java/com/androidpractice/abha/carouselpicker/MainActivity.java
+++ app/src/main/java/com/androidpractice/abha/carouselpicker/MainActivity.java
@@ -3,6 +3,8 @@ package com.androidpractice.abha.carouselpicker;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -10,9 +12,11 @@ import java.util.List;
 
 import in.goodiebag.carouselpicker.CarouselPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     CarouselPicker carouselPicker1;
+    Button btnPrev1, btnNext1;
+    private int imageSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         carouselPicker1 = (CarouselPicker) findViewById(R.id.carouselPicker1);
+        btnPrev1 = (Button) findViewById(R.id.btnPrev1);
+        btnNext1 = (Button) findViewById(R.id.btnNext1);
+
+        btnPrev1.setOnClickListener(this);
+        btnNext1.setOnClickListener(this);
 
         // Carousel with all images
         List<CarouselPicker.PickerItem> itemImages = new ArrayList<>();
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 Toast.makeText(getApplicationContext(), "Position selected: " + position, Toast.LENGTH_SHORT).show();
+                imageSelected = position;
             }
 
             @Override
@@ -47,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnPrev1:
+                Toast.makeText(this, "Previous", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnNext1:
+                Toast.makeText(this, "Selected image position is: " + imageSelected, Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
